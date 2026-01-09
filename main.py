988072c58be83d7d1351175b47ec373f4e10a666
Tam <5542463+Tam-d@users.noreply.github.com>
2026-01-09 02:57:08 +0000

    CH2L4: managing fps

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    game_clock = pygame.time.Clock()
+    delta_time = 0
+
     while(1):
         log_state()
 
@@ -20,6 +23,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        delta_time += game_clock.tick(60) /1000
 
 
 if __name__ == "__main__":
