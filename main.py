ee813fa103fe4251586b3b751320ce53b52daafc
Tam <5542463+Tam-d@users.noreply.github.com>
2026-01-12 18:14:22 +0000

    CH4L5: add collision of shots and asteroids

--- main.py
+++ main.py
@@ -55,6 +55,13 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
+
         for d in drawable:
             d.draw(screen)
 
