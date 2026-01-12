a0723c23035d66e0b734998cfcbfe44ebfa57944
Tam <5542463+Tam-d@users.noreply.github.com>
2026-01-12 17:57:11 +0000

    CH4L4: add rate limit to shots

--- player.py
+++ player.py
@@ -1,13 +1,14 @@
 import pygame
 from circleshape import CircleShape
 from shot import Shot
-from constants import PLAYER_RADIUS, PLAYER_SHOOT_SPEED, LINE_WIDTH, PLAYER_SPEED, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS, LINE_WIDTH, PLAYER_SPEED, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
 
     def __init__(self, x, y):
         super().__init__(x,y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
@@ -23,6 +24,8 @@ class Player(CircleShape):
 
     def update(self, delta_time):
         keys = pygame.key.get_pressed()
+        self.shot_cooldown -= delta_time
+        print(f"shot cool down timer: {self.shot_cooldown}")
 
         if keys[pygame.K_a]:
             self.rotate(-delta_time)
@@ -33,9 +36,15 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-delta_time)
         if keys[pygame.K_SPACE]:
-            self.shoot()
-
+            if self.shot_cooldown > 0:
+                print("shot on cooldown")
+            else:
+                self.shoot()
+                self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
+                print(f"Setting cooldown: {self.shot_cooldown}")
+                
     def shoot(self):
+        print("Shot a shot")
         shot = Shot(self.position.x, self.position.y)
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
